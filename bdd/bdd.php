f77ee1374464f0d2166e0e5baa553c2c1f90fd30
davy <davy@VMARE.com>
2025-12-01 21:45:48 +0100

    add script for bdd and sql

---
+++ bdd/bdd.php
@@ -0,0 +1,15 @@
+// bdd/bdd.php gère la connexion à la base de données
+
+<?php
+
+try {
+    $users = "admin";  // put your database username here
+    $pass = "myadmin";   // put your database password here
+    $bdd = new PDO("mysql:host=localhost;dbname=fitconnect;charset=utf8", $users, $pass);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur ! : " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
